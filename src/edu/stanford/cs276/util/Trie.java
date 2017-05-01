2e6a79e37c6a9ef14447061cc9ec918ee193dd0f
FeiLiu <liufei11111@hotmail.com>
2017-04-30 22:56:29 -0700

    nonempty map

--- src/edu/stanford/cs276/util/Trie.java
+++ src/edu/stanford/cs276/util/Trie.java
@@ -81,7 +81,7 @@ class TrieNode implements Serializable{
 
   public static synchronized TrieNode deserialize(byte[]  bytesArray, int startI, int endI){
     if (startI==endI){
-      return null;
+      return new HashMap<>(1);
     }
     System.out.println("TrieNode deserial: start1: "+startI+", endI: "+endI);
     int numOfMetaBytes = 4*4;
