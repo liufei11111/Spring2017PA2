d2e65b1931313f16fe51a4e3841f6b44b49c5ff3
FeiLiu <liufei11111@hotmail.com>
2017-04-30 23:17:15 -0700

    more memory to build

--- buildmodels.sh
+++ buildmodels.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 # ./buildmodels.sh <training corpus dir> <training edit1s file>
 
-java -Xmx2048m -cp classes edu.stanford.cs276.BuildModels $1 $2
+java -Xmx3072m -cp classes edu.stanford.cs276.BuildModels $1 $2
 
