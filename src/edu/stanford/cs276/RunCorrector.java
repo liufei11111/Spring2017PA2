d454bdb5817d38921007beeb5d8e2f41debd0434
FeiLiu <liufei11111@hotmail.com>
2017-05-02 02:57:56 -0700

    debug 2

--- src/edu/stanford/cs276/RunCorrector.java
+++ src/edu/stanford/cs276/RunCorrector.java
@@ -114,7 +114,7 @@ public class RunCorrector {
        * IMPORTANT: In your final submission DO NOT add any additional print statements as 
        * this will interfere with the autograder
        */
-      System.out.println(correctedQuery);
+      System.out.println(correctedQuery.getFirst());
     }
     fw.close();
     queriesFileReader.close();
