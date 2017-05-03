3a6b034fcdda7bd1b778dc25ec6658b0aa0b68ea
FeiLiu <liufei11111@hotmail.com>
2017-05-03 03:17:57 -0700

    Revert "quick run"
    This reverts commit a2d682ae2ca2f122ebc24f1225f290ebae4381a7.

--- src/edu/stanford/cs276/Config.java
+++ src/edu/stanford/cs276/Config.java
@@ -5,10 +5,10 @@ public class Config {
   public static final String languageModelFile = "languageModel";
   public static final int distance = 2; // number of edit's allowed in query
   public static final int correctionDistance = 2; // for a term, edit distance
-  public static final double smoothingFactor = 0.1;
-  public static final double languageModelScalingFactor = 1.0;
+  public static final double smoothingFactor = 0.01;
+  public static final double languageModelScalingFactor = 0.1;
   public static final double eps = 1e-25;
-  public static final int candidateSetSize = 3;
-  public static final double charEditProb = 1-1e3;// given that it is selected
-  public static final Double logNoOpProb = Math.log(charEditProb);
+  public static final int candidateSetSize = 10;
+//  public static final double charEditProb = 1-1e32;// given that it is selected
+  public static final Double logNoOpProb = -125.0;
 }
