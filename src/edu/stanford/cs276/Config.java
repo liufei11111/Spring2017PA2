ce4544db5b094a9495fe660880cc3bbe7b310940
FeiLiu <liufei11111@hotmail.com>
2017-05-03 01:17:35 -0700

    assumptions

--- src/edu/stanford/cs276/Config.java
+++ src/edu/stanford/cs276/Config.java
@@ -3,16 +3,12 @@ package edu.stanford.cs276;
 public class Config {
   public static final String noisyChannelFile = "noisyChannel";
   public static final String languageModelFile = "languageModel";
-  public static final String trieFile = "trieFile";
-  public static final String candidateGenFile = "candidateGenerator";
   public static final int distance = 2; // number of edit's allowed in query
   public static final int correctionDistance = 2; // for a term, edit distance
-  public static final int kOfGrams = 2;
   public static final double smoothingFactor = 0.1;
   public static final double languageModelScalingFactor = 1.0;
   public static final double eps = 1e-25;
-  public static final int hashMapInitialSize = 8;
-  public static final int candidateSetSize = 300;
+  public static final int candidateSetSize = 30;
   public static final double charEditProb = 0.05;// arbitrary assumption.
   public static final Double logNoOpProb = Math.log(1-charEditProb);
 }
