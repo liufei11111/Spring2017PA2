39bb3991b0a3221354e460add192f2e985641ba1
FeiLiu <liufei11111@hotmail.com>
2017-05-03 01:56:13 -0700

    not getting it

--- src/edu/stanford/cs276/Config.java
+++ src/edu/stanford/cs276/Config.java
@@ -3,7 +3,7 @@ package edu.stanford.cs276;
 public class Config {
   public static final String noisyChannelFile = "noisyChannel";
   public static final String languageModelFile = "languageModel";
-  public static final int distance = 1; // number of edit's allowed in query
+  public static final int distance = 2; // number of edit's allowed in query
   public static final int correctionDistance = 2; // for a term, edit distance
   public static final double smoothingFactor = 0.1;
   public static final double languageModelScalingFactor = 1.0;
