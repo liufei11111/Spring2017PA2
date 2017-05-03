90037532a5121d56f812bd46783930f19eaf41f3
FeiLiu <liufei11111@hotmail.com>
2017-05-02 23:08:54 -0700

    2*2 edits

--- src/edu/stanford/cs276/Config.java
+++ src/edu/stanford/cs276/Config.java
@@ -5,12 +5,12 @@ public class Config {
   public static final String languageModelFile = "languageModel";
   public static final String trieFile = "trieFile";
   public static final String candidateGenFile = "candidateGenerator";
-  public static final int distance = 3; // number of edit's allowed in query
-  public static final int correctionDistance = 3; // for a term, edit distance
+  public static final int distance = 2; // number of edit's allowed in query
+  public static final int correctionDistance = 2; // for a term, edit distance
   public static final int kOfGrams = 2;
-  public static final double smoothingFactor = 0.1;
+  public static final double smoothingFactor = 0.5;
   public static final double languageModelScalingFactor = 1.0;
-  public static final double eps = 1e-250;
+  public static final double eps = 1e-25;
   public static final int hashMapInitialSize = 8;
   public static final int candidateSetSize = 10;
   public static final double charEditProb = 0.05;// arbitrary assumption.
