53cbaa742990ab73212df3c6db09eed036718a40
FeiLiu <liufei11111@hotmail.com>
2017-05-03 02:27:15 -0700

    tune it

--- src/edu/stanford/cs276/Config.java
+++ src/edu/stanford/cs276/Config.java
@@ -6,9 +6,9 @@ public class Config {
   public static final int distance = 2; // number of edit's allowed in query
   public static final int correctionDistance = 2; // for a term, edit distance
   public static final double smoothingFactor = 0.1;
-  public static final double languageModelScalingFactor = 1.0;
+  public static final double languageModelScalingFactor = 0.1;
   public static final double eps = 1e-25;
   public static final int candidateSetSize = 10;
-  public static final double charEditProb = 1-1e25;// given that it is selected
-  public static final Double logNoOpProb = Math.log(1-charEditProb);
+//  public static final double charEditProb = 1-1e32;// given that it is selected
+  public static final Double logNoOpProb = -25.0;
 }
