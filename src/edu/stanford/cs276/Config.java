0cd48a969316128f6e78810adf89ff4ea38d94db
FeiLiu <liufei11111@hotmail.com>
2017-05-03 01:53:09 -0700

    candidate and push error

--- src/edu/stanford/cs276/Config.java
+++ src/edu/stanford/cs276/Config.java
@@ -8,7 +8,7 @@ public class Config {
   public static final double smoothingFactor = 0.1;
   public static final double languageModelScalingFactor = 1.0;
   public static final double eps = 1e-25;
-  public static final int candidateSetSize = 3;
-  public static final double charEditProb = 0.99;// given that it is selected
+  public static final int candidateSetSize = 10;
+  public static final double charEditProb = 0.999999;// given that it is selected
   public static final Double logNoOpProb = Math.log(1-charEditProb);
 }
