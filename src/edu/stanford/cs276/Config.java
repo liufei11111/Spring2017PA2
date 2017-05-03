e13a7c1f1723574ac04aead5f83b554483f5423d
FeiLiu <liufei11111@hotmail.com>
2017-05-02 21:27:12 -0700

    who knows 6

--- src/edu/stanford/cs276/Config.java
+++ src/edu/stanford/cs276/Config.java
@@ -5,14 +5,14 @@ public class Config {
   public static final String languageModelFile = "languageModel";
   public static final String trieFile = "trieFile";
   public static final String candidateGenFile = "candidateGenerator";
-  public static final int distance = 1; // number of edit's allowed in query
-  public static final int correctionDistance = 1; // for a term, edit distance
+  public static final int distance = 2; // number of edit's allowed in query
+  public static final int correctionDistance = 2; // for a term, edit distance
   public static final int kOfGrams = 2;
   public static final double smoothingFactor = 0.1;
   public static final double languageModelScalingFactor = 1.0;
   public static final double eps = 1e-250;
   public static final int hashMapInitialSize = 8;
   public static final int candidateSetSize = 10;
-  public static final double charEditProb = 0.01;// arbitrary assumption.
-  public static final Double logNoOpProb = Math.log(0.95);
+  public static final double charEditProb = 0.05;// arbitrary assumption.
+  public static final Double logNoOpProb = Math.log(1-charEditProb);
 }
