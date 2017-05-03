f0ca56fd6a4ddb7a71e4aa126fae0591f7f87d52
FeiLiu <liufei11111@hotmail.com>
2017-05-03 01:57:31 -0700

    desperate

--- src/edu/stanford/cs276/util/IntegerDoublePairDecendingComparator.java
+++ src/edu/stanford/cs276/util/IntegerDoublePairDecendingComparator.java
@@ -9,6 +9,6 @@ public class IntegerDoublePairDecendingComparator implements Comparator<Pair<Int
 
   @Override
   public int compare(Pair<Integer, Double> o1, Pair<Integer, Double> o2) {
-    return o2.getSecond().compareTo(o1.getSecond());
+    return o1.getSecond().compareTo(o2.getSecond());
   }
 }
