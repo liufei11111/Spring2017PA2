522a0ebb99b95fbcc4845f25851b6e1f6b83fe0a
FeiLiu <liufei11111@hotmail.com>
2017-05-02 20:51:45 -0700

    who knows 1

--- src/edu/stanford/cs276/CandidateGenerator.java
+++ src/edu/stanford/cs276/CandidateGenerator.java
@@ -66,7 +66,7 @@ public class CandidateGenerator implements Serializable {
     for(int i=0;i<terms.length-1;++i){
       if (i==0) {
         // raw count is good as the total count is a constant
-        int rawCount = lm.rawCountForTerm(terms[i];
+        int rawCount = lm.rawCountForTerm(terms[i]);
         logLanguageScore += Math.log(rawCount+Config.eps);
       }
       int rawBiCount = lm.rawBiCountForTerms(terms[i],terms[i+1]);
