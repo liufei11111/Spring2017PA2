af0e936c269cf380e62c414e08640ce8dc10185a
FeiLiu <liufei11111@hotmail.com>
2017-05-04 00:10:17 -0700

    fast run

--- src/edu/stanford/cs276/Config.java
+++ src/edu/stanford/cs276/Config.java
@@ -8,11 +8,11 @@ public class Config {
   public static final int distance = 2; // number of edit's allowed in query
   public static final int correctionDistance = 2; // for a term, edit distance
   public static final int kOfGrams = 2;
-  public static final double smoothingFactor = 0.5;
+  public static final double smoothingFactor = 0.9;
   public static final double languageModelScalingFactor = 1.0;
   public static final double eps = 1e-25;
   public static final int hashMapInitialSize = 8;
-  public static final int candidateSetSize = 10;
+  public static final int candidateSetSize = 3;
   public static final double charEditProb = 0.05;// arbitrary assumption.
   public static final Double logNoOpProb = Math.log(1-charEditProb);
 }
